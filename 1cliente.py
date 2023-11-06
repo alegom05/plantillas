71d58056ad3a72dd3d7d22ed61b50aef6955f78c
alegom05 <146905500+alegom05@users.noreply.github.com>
2023-11-06 13:16:59 -0500

    1cliente.py

--- 1cliente.py
+++ 1cliente.py
@@ -1,34 +1,18 @@
 import socket
 
-host = '127.0.0.1'
-port = 5001
+SOCK_BUFFER = 1024
 
-if __name__=='__main__':
-
+if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('0.0.0.0',5001)
+    server_address = ('localhost', 5001)
 
-    print(f"Iniciando servidor en: {server_address[0]}:{server_address[1]}")
-    sock.bind((host, port))
-
-    sock.listen(5)
-
-    print(f"Servidor de eco escuchando en {host}:{port}")
-
-    while True:
-        conn, client_address = sock.accept()
-        print(f"Conexión entrante desde {client_address}")
-
-        #try:
-        #except KeyboardInterrupt:
-        #   print("Interrupción")
-        #finally:
-        #   conn.close
-
-    #conn.close()
+    print(f"Conectando al servidor {server_address[0]}:{server_address[1]}")
+    sock.connect(server_address)
 
     #envia
-    # conn.sendall(codigo.encode('utf-8'))
+    # sock.sendall(a.encode('utf-8'))
 
     #recibe
-    # dato=conn.recv(1024).decode('utf-8')
+    # rpta=sock.recv(SOCK_BUFFER).decode('utf-8')
+
+    # sock.close()
