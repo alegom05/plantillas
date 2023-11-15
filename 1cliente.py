0b531a8ca9788aacca0044d109bf9167db871937
alegom05 <146905500+alegom05@users.noreply.github.com>
2023-11-15 17:29:11 -0500

    Add files via upload

---
+++ 1cliente.py
@@ -0,0 +1,22 @@
+import socket
+
+SOCK_BUFFER = 1024
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 5001)
+
+    print(f"Conectando al servidor {server_address[0]}:{server_address[1]}")
+    sock.connect(server_address)
+
+    #envia
+    # sock.sendall(a.encode('utf-8'))
+
+    #recibe
+    # rpta=sock.recv(SOCK_BUFFER).decode('utf-8')
+    # if not rpta:
+    #   break
+
+    # sock.close()
+
+
---
+++ 1servidor.py
@@ -0,0 +1,38 @@
+import socket
+
+host = '127.0.0.1'
+port = 5001
+
+if __name__=='__main__':
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('0.0.0.0',5001)
+
+    print(f"Iniciando servidor en: {server_address[0]}:{server_address[1]}")
+    sock.bind((host, port))
+
+    sock.listen(5)
+
+    print(f"Servidor escuchando en {host}:{port}")
+
+    while True:
+        conn, client_address = sock.accept()
+        print(f"Conexión entrante desde {client_address}")
+
+        #try:
+        #except KeyboardInterrupt:
+        #   print("Interrupción")
+        #finally:
+        #   conn.close
+
+    #conn.close()
+
+
+    #envia
+    # conn.sendall(dato.encode('utf-8'))
+
+    #recibe
+    # rpta=conn.recv(1024).decode('utf-8')
+
+    #convertir a cadena de texto
+    # str()
